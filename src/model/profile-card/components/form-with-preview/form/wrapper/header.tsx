deb128d567b45934165dedbc4c7f1efeb4a16374
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-23 08:33:42 +0900

    feat: enhance ProfileCardFormHeader with delete, reset, and copy functionality

--- src/model/profile-card/components/form-with-preview/form/wrapper/header.tsx
+++ src/model/profile-card/components/form-with-preview/form/wrapper/header.tsx
@@ -1,24 +1,59 @@
 "use client";
 
 import { Divider, Flex, Title } from "@mantine/core";
+import { useRouter } from "next/navigation";
 import type { FC } from "react";
 
 import { FormMenuButton } from "@/model/common/components/form-menu";
-import { useIsNewItemPagePath } from "@/model/common/hooks/is-new-item-page-path";
+import { ModelPathMapping } from "@/model/common/const";
+import { NEW_ITEM_ID } from "@/model/common/const/key";
+import { useContentIdFromPagePath } from "@/model/common/hooks/model-page-path";
+
+import { profileCardConverter } from "../../converter";
+import { useProfileCardFormServerState } from "../../server-state/hook";
+import { PROFILE_CARD_FORM_STORE_INITIAL_STATE } from "../../store/const";
+import { useProfileCardFormStore } from "../../store/hook";
+import { deleteProfileCard } from "./operation/delete/query";
 
 export const ProfileCardFormHeader: FC = () => {
-  const isNewItemPagePath = useIsNewItemPagePath("ProfileCard");
-  const title = `プロフィールカード ${isNewItemPagePath ? "新規作成" : "詳細"}`;
+  const contentId = useContentIdFromPagePath("ProfileCard");
+  const router = useRouter();
+  const serverState = useProfileCardFormServerState();
+  const setFormValue = useProfileCardFormStore((state) => state.setFormValue);
+
+  const isNewItem = contentId === NEW_ITEM_ID;
+  const title = `プロフィールカード ${isNewItem ? "新規作成" : "詳細"}`;
+
+  const isDraft = serverState?.creationStatus === "DRAFT";
+
+  const onDelete = async () => {
+    await deleteProfileCard(contentId);
+  };
+
+  const onReset = () => {
+    setFormValue(PROFILE_CARD_FORM_STORE_INITIAL_STATE);
+    if (!isNewItem) {
+      // もともとの値を復元する
+      setFormValue(profileCardConverter.toClient(serverState));
+    }
+  };
+
+  const onCopyAndNew = () => {
+    router.push(
+      `${ModelPathMapping.ProfileCard.idToPath(NEW_ITEM_ID)}?base=${contentId}`,
+    );
+  };
 
   return (
     <>
       <Flex justify="space-between" align="center" p="lg">
         <Title order={3}>{title}</Title>
         <FormMenuButton
-          // TODO: 実装
-          onDelete={() => {}}
-          onReset={() => {}}
-          isDeletable={false}
+          onDelete={onDelete}
+          onReset={onReset}
+          isDeletable={isDraft}
+          isCopyable={!isNewItem}
+          onCopyAndNew={onCopyAndNew}
         />
       </Flex>
       <Divider />
