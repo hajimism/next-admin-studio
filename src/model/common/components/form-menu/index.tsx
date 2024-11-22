8c409b746404dd3a8d9c6db4c5be01ecfb0c05b3
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-23 08:33:27 +0900

    feat: add 'コピーして新規作成' option to FormMenuButton for creating new profile cards

--- src/model/common/components/form-menu/index.tsx
+++ src/model/common/components/form-menu/index.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { ActionIcon, Menu, Text, rem } from "@mantine/core";
-import { IconDotsVertical, IconRefresh, IconTrash } from "@tabler/icons-react";
+import {
+  IconCopy,
+  IconDotsVertical,
+  IconRefresh,
+  IconTrash,
+} from "@tabler/icons-react";
 import type { FC } from "react";
 
 import { openConfirmModal } from "@/common/lib/modal";
@@ -24,18 +29,31 @@ const MODAL_PROPS = {
     labels: { confirm: "リセット", cancel: "キャンセル" },
     centered: true,
   },
+  ON_COPY_AND_NEW: {
+    title: (
+      <Text fw="bold">
+        このコンテンツをもとに新しいプロフィールカードを作りますか？
+      </Text>
+    ),
+    labels: { confirm: "コピーして新規作成", cancel: "キャンセル" },
+    centered: true,
+  },
 };
 
 type Props = {
   onDelete: () => void;
   onReset: () => void;
+  onCopyAndNew: () => void;
   isDeletable: boolean;
+  isCopyable: boolean;
 };
 
 export const FormMenuButton: FC<Props> = ({
   onDelete,
   onReset,
   isDeletable,
+  onCopyAndNew,
+  isCopyable,
 }) => {
   return (
     <Menu position="left-start" offset={2}>
@@ -72,6 +90,18 @@ export const FormMenuButton: FC<Props> = ({
           }
         >
           削除
+        </Menu.Item>
+        <Menu.Item
+          disabled={!isCopyable}
+          onClick={() => {
+            openConfirmModal({
+              ...MODAL_PROPS.ON_COPY_AND_NEW,
+              onConfirm: () => onCopyAndNew(),
+            });
+          }}
+          leftSection={<IconCopy style={{ width: rem(14), height: rem(14) }} />}
+        >
+          コピーして新規作成
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>
