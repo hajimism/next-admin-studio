59b856ffab2f2f5327719b82e4c7bba3a4800fb0
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-22 20:12:00 +0900

    feat: refactor breadcrumbs to use 'current' prop instead of 'adminLabel' for improved clarity

--- src/model/common/components/breadcrumbs/index.tsx
+++ src/model/common/components/breadcrumbs/index.tsx
@@ -5,20 +5,18 @@ import type { FC } from "react";
 import { MODELS, type ModelName, ModelPathMapping } from "../../const";
 
 type Props = {
-  adminLabel: string;
+  current: string;
   modelName: ModelName;
 };
 
-export const DetailPageBreadcrumbs: FC<Props> = ({ adminLabel, modelName }) => {
+export const DetailPageBreadcrumbs: FC<Props> = ({ current, modelName }) => {
   return (
     <MantineBreadcrumbs>
       <Anchor href={ModelPathMapping[modelName].indexPath} component={Link}>
         {MODELS[modelName].label}一覧
       </Anchor>
       <Text>
-        {truncateText(adminLabel) || (
-          <span className="text-gray-5">Untitled</span>
-        )}
+        {truncateText(current) || <span className="text-gray-5">Untitled</span>}
       </Text>
     </MantineBreadcrumbs>
   );
--- src/model/profile-card/components/form-with-preview/breadcrumbs/index.tsx
+++ src/model/profile-card/components/form-with-preview/breadcrumbs/index.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { Text } from "@mantine/core";
-
 import { DetailPageBreadcrumbs } from "@/model/common/components/breadcrumbs";
-import { MODELS } from "@/model/common/const";
 import { useIsNewItemPagePath } from "@/model/common/hooks/is-new-item-page-path";
 
 import { useProfileCardFormStore } from "../store/hook";
@@ -12,10 +9,7 @@ export const ProfileCardFormWithPreviewBreadcrumbs = () => {
   const isNewItemPagePath = useIsNewItemPagePath("ProfileCard");
   const adminLabel = useProfileCardFormStore((state) => state.adminLabel);
 
-  if (isNewItemPagePath)
-    return <Text c="gray.6">{MODELS.ProfileCard.label} 新規作成</Text>;
+  const current = isNewItemPagePath ? "新規作成" : adminLabel;
 
-  return (
-    <DetailPageBreadcrumbs modelName="ProfileCard" adminLabel={adminLabel} />
-  );
+  return <DetailPageBreadcrumbs modelName="ProfileCard" current={current} />;
 };
