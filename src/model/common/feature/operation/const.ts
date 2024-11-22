37a6e78f7bc849289bf45a1b87c9cfff3d253529
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-23 08:33:17 +0900

    feat: rename 'REPURPOSE' operation to 'COPY_AND_NEW' for better clarity

--- src/model/common/feature/operation/const.ts
+++ src/model/common/feature/operation/const.ts
@@ -1,7 +1,7 @@
 export const CONTENT_OPERATIONS = [
   "CREATE_CONFIRMED",
   "DELETE",
-  "REPURPOSE",
+  "COPY_AND_NEW",
   "CREATE_DRAFT",
   "CONFIRM_DRAFT",
   "EDIT_CONFIRMED",
@@ -13,7 +13,7 @@ export const CONTENT_OPERATIONS = [
 export const CONTENT_OPERATION_NAME_MAPPING = {
   CREATE_CONFIRMED: { name: "確定保存" },
   DELETE: { name: "削除" },
-  REPURPOSE: { name: "複製" },
+  COPY_AND_NEW: { name: "コピーして新規作成" },
   CREATE_DRAFT: { name: "下書き保存" },
   CONFIRM_DRAFT: { name: "確定保存" },
   EDIT_CONFIRMED: { name: "更新" },
