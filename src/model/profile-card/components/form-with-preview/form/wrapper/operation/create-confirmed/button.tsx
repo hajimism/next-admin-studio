00e1bc18adc74798c66ac4338527452a90146379
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-22 19:50:56 +0900

    feat: enhance form submission handling with validation and feedback in ProfileCardFormCreateConfirmedButton

--- src/model/profile-card/components/form-with-preview/form/wrapper/operation/create-confirmed/button.tsx
+++ src/model/profile-card/components/form-with-preview/form/wrapper/operation/create-confirmed/button.tsx
@@ -4,10 +4,15 @@ import type { FC } from "react";
 import { useProfileCardFormCreateConfirmed } from "./hook";
 
 export const ProfileCardFormCreateConfirmedButton: FC = () => {
-  const { loading, disabled, onClick } = useProfileCardFormCreateConfirmed();
+  const { loading, disabled, formAction } = useProfileCardFormCreateConfirmed();
 
   return (
-    <Button loading={loading} disabled={disabled} onClick={onClick}>
+    <Button
+      loading={loading}
+      disabled={disabled}
+      formAction={formAction}
+      type="submit"
+    >
       確定保存
     </Button>
   );
--- src/model/profile-card/components/form-with-preview/form/wrapper/operation/create-confirmed/hook.ts
+++ src/model/profile-card/components/form-with-preview/form/wrapper/operation/create-confirmed/hook.ts
@@ -1,18 +1,59 @@
+import { errorToast, loadingToast } from "@/common/components/form/toast";
 import { useCallback } from "react";
-import { useProfileCardFormOperationState } from "../../../../store/hook";
+import {
+  useProfileCardFormOperationState,
+  useProfileCardFormStore,
+} from "../../../../store/hook";
 
 export const useProfileCardFormCreateConfirmed = () => {
   const { loading, disabled, startOperation } =
     useProfileCardFormOperationState("CREATE_CONFIRMED");
+  const getFormIsValid = useProfileCardFormStore(
+    (state) => state.getFormIsValid,
+  );
+  const setValidationPhase = useProfileCardFormStore(
+    (state) => state.setValidationPhase,
+  );
 
-  const handleClick = useCallback(async () => {
+  const formAction = useCallback(async () => {
+    // バリデーションフェーズの更新
+    setValidationPhase("onConfirmedSubmit");
+
+    // クライアント側でのバリデーション
+    if (!getFormIsValid()) {
+      errorToast({
+        title: "入力エラー",
+        message: "入力内容の修正が必要です😥",
+      });
+      return;
+    }
+
+    // フォームアクションの開始
     const { settleOperation } = startOperation();
+    // ユーザーにフィードバック
+    const { settle: settleToast, error: loadingErrorToast } = loadingToast({
+      message: "コンテンツを確定保存しています",
+    });
 
-    // 2秒待つ
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    // TODO
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // TODO
+    const isError = false;
+    if (isError) {
+      loadingErrorToast({
+        message: "コンテンツの確定保存に失敗しました",
+      });
+    }
 
     settleOperation();
-  }, [startOperation]);
+    settleToast({
+      message: "確定保存が完了しました！",
+    });
 
-  return { loading, disabled, onClick: handleClick };
+    // バリデーションフェーズのリセット
+    setValidationPhase("onChange");
+  }, [startOperation, getFormIsValid, setValidationPhase]);
+
+  return { loading, disabled, formAction };
 };
