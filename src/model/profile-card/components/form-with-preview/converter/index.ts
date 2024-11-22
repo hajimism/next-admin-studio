6a8ec066726604be2acf93e07334ead5f148e4b4
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-22 16:33:14 +0900

    feat: add initial state constant for profile card form and update converter to use it

--- src/model/profile-card/components/form-with-preview/converter/index.ts
+++ src/model/profile-card/components/form-with-preview/converter/index.ts
@@ -1,11 +1,12 @@
 import type { ProfileCardFormServerState } from "../server-state/type";
+import { PROFILE_CARD_FORM_STORE_INITIAL_STATE } from "../store/const";
 import type { ProfileCardForm } from "../store/type";
 
 export const profileCardConverter = {
   toClient: (
     serverState: ProfileCardFormServerState | undefined,
-  ): ProfileCardForm | undefined => {
-    if (!serverState) return undefined;
+  ): ProfileCardForm => {
+    if (!serverState) return PROFILE_CARD_FORM_STORE_INITIAL_STATE;
 
     return {
       ...serverState,
