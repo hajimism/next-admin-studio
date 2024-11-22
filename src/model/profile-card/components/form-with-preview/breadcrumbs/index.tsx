1ee7f08ffaa6fb99f6950f254c598b0e7505596f
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-22 15:21:50 +0900

    feat: enhance ProfileCardForm components to display new item creation status in breadcrumbs and header

--- src/model/profile-card/components/form-with-preview/breadcrumbs/index.tsx
+++ src/model/profile-card/components/form-with-preview/breadcrumbs/index.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { Text } from "@mantine/core";
+
 import { DetailPageBreadcrumbs } from "@/model/common/components/breadcrumbs";
+import { MODELS } from "@/model/common/const";
+import { useIsNewItemPagePath } from "@/model/common/hooks/is-new-item-page-path";
+
 import { useProfileCardFormStore } from "../store/hook";
 
 export const ProfileCardFormWithPreviewBreadcrumbs = () => {
+  const isNewItemPagePath = useIsNewItemPagePath("ProfileCard");
   const adminLabel = useProfileCardFormStore((state) => state.adminLabel);
+
+  if (isNewItemPagePath)
+    return <Text c="gray.6">{MODELS.ProfileCard.label} 新規作成</Text>;
 
   return (
     <DetailPageBreadcrumbs modelName="ProfileCard" adminLabel={adminLabel} />
--- src/model/profile-card/components/form-with-preview/form/wrapper/header.tsx
+++ src/model/profile-card/components/form-with-preview/form/wrapper/header.tsx
@@ -4,12 +4,16 @@ import { Divider, Flex, Title } from "@mantine/core";
 import type { FC } from "react";
 
 import { FormMenuButton } from "@/model/common/components/form-menu";
+import { useIsNewItemPagePath } from "@/model/common/hooks/is-new-item-page-path";
 
 export const ProfileCardFormHeader: FC = () => {
+  const isNewItemPagePath = useIsNewItemPagePath("ProfileCard");
+  const title = `プロフィールカード ${isNewItemPagePath ? "新規作成" : "詳細"}`;
+
   return (
     <>
       <Flex justify="space-between" align="center" p="lg">
-        <Title order={3}>プロフィールカード </Title>
+        <Title order={3}>{title}</Title>
         <FormMenuButton
           // TODO: 実装
           onDelete={() => {}}
