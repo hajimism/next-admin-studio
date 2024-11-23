52bc050fb12ee935e36df2e5d5ea1b18934a202c
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-23 11:01:07 +0900

    feat: replace DetailPageBreadcrumbs with ProfileCardFormWithPreviewBreadcrumbs for profile card form

--- src/model/profile-card/components/form-with-preview/breadcrumbs/index.tsx
+++ src/model/profile-card/components/form-with-preview/breadcrumbs/index.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import { DetailPageBreadcrumbs } from "@/model/common/components/breadcrumbs";
+import { Anchor, Breadcrumbs, Text } from "@mantine/core";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { MODELS } from "@/model/common/const";
 import { NEW_ITEM_ID } from "@/model/common/const/key";
-import { useContentIdFromPagePath } from "@/model/common/hooks/model-page-path";
+import { profileCardPathMapping } from "@/model/profile-card/lib/path";
+
+import { truncateText } from "@/common/lib/truncate-text";
 
 import { useProfileCardFormStore } from "../store/hook";
 
 export const ProfileCardFormWithPreviewBreadcrumbs = () => {
-  const contentId = useContentIdFromPagePath("ProfileCard");
-  const isNewItemPagePath = contentId === NEW_ITEM_ID;
+  const pathname = usePathname();
   const adminLabel = useProfileCardFormStore((state) => state.adminLabel);
 
+  const contentId = profileCardPathMapping.pathToId(pathname);
+  const isNewItemPagePath = contentId === NEW_ITEM_ID;
   const current = isNewItemPagePath ? "新規作成" : adminLabel;
 
-  return <DetailPageBreadcrumbs modelName="ProfileCard" current={current} />;
+  return (
+    <Breadcrumbs>
+      <Anchor href={profileCardPathMapping.indexPath} component={Link}>
+        {MODELS.ProfileCard.label}一覧
+      </Anchor>
+      <Text>
+        {truncateText(current) || <span className="text-gray-5">Untitled</span>}
+      </Text>
+    </Breadcrumbs>
+  );
 };
