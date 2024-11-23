715e38718cec05a8adc348bdf91371199ad10514
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-23 09:18:24 +0900

    feat: refactor NewProfileCardPage to include ProfileCardFormStoreProvider and add copy-and-new parameters handling

--- src/app/(general)/profile-cards/new/page.tsx
+++ src/app/(general)/profile-cards/new/page.tsx
@@ -1,5 +1,26 @@
-import { ProfileCardFormWithPreview } from "@/model/profile-card/components/form-with-preview";
+import type { SearchParams } from "nuqs";
 
-export default async function NewProfileCardPage() {
-  return <ProfileCardFormWithPreview />;
+import { copyAndNewParamsCache } from "@/model/common/params/copy-and-new";
+import { ProfileCardFormWithPreview } from "@/model/profile-card/components/form-with-preview";
+import { profileCardConverter } from "@/model/profile-card/components/form-with-preview/converter";
+import { getProfileCard } from "@/model/profile-card/components/form-with-preview/query";
+import { PROFILE_CARD_FORM_STORE_INITIAL_STATE } from "@/model/profile-card/components/form-with-preview/store/const";
+import { ProfileCardFormStoreProvider } from "@/model/profile-card/components/form-with-preview/store/provider";
+
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default async function NewProfileCardPage(props: Props) {
+  const { base } = await copyAndNewParamsCache.parse(props.searchParams);
+
+  const initialState = base
+    ? profileCardConverter.toClient(await getProfileCard(base))
+    : PROFILE_CARD_FORM_STORE_INITIAL_STATE;
+
+  return (
+    <ProfileCardFormStoreProvider initialState={initialState}>
+      <ProfileCardFormWithPreview />
+    </ProfileCardFormStoreProvider>
+  );
 }
