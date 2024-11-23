be222b06f7325ff7244e6d86aa52c7b7adfcfed8
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-23 09:19:57 +0900

    feat: migrate ProfileCardDetailLayout logic to ProfileCardDetailPage for improved structure

--- src/app/(general)/profile-cards/[id]/page.tsx
+++ src/app/(general)/profile-cards/[id]/page.tsx
@@ -1,5 +1,23 @@
 import { ProfileCardFormWithPreview } from "@/model/profile-card/components/form-with-preview";
+import { ProfileCardFormServerStateProviderContainer } from "@/model/profile-card/components/form-with-preview/server-state";
+import { ProfileCardFormStoreProvider } from "@/model/profile-card/components/form-with-preview/store/provider";
 
-export default async function ProfileCardDetailPage() {
-  return <ProfileCardFormWithPreview />;
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function ProfileCardDetailPage(props: Props) {
+  const { id } = await props.params;
+
+  return (
+    <ProfileCardFormServerStateProviderContainer id={id}>
+      {(formValue) => (
+        <ProfileCardFormStoreProvider initialState={formValue}>
+          <ProfileCardFormWithPreview />
+        </ProfileCardFormStoreProvider>
+      )}
+    </ProfileCardFormServerStateProviderContainer>
+  );
 }
