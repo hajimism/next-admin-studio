6f8e9896e6e0ef8929fc817f2347d26336bd6fd4
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 10:05:22 +0900

    feat: update Tailwind CSS configuration to limit content scanning to TypeScript files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ for (const [name, colors] of Object.entries(DEFAULT_THEME.colors)) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./stories/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
