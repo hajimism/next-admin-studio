1c32b62034ba78416a1a4fc7d873c1d7a2a3cb09
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 14:04:08 +0900

    refactor: remove id from PROFILE_CARD_FORM_STORE_INITIAL_STATE for improved consistency

--- src/model/profile-card/components/form-with-preview/store/const.ts
+++ src/model/profile-card/components/form-with-preview/store/const.ts
@@ -1,7 +1,6 @@
 import type { ProfileCardForm } from "../form/type";
 
 export const PROFILE_CARD_FORM_STORE_INITIAL_STATE: ProfileCardForm = {
-  id: "",
   adminLabel: "",
   luckyNumber: 0,
   name: "",
