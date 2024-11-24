d89730009681c6eeb819fa640945d4fab54f3c80
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 15:15:26 +0900

    refactor: rename type.ts to form.ts and restructure validation logic; add search input slices

--- src/model/common/store/form.ts
+++ src/model/common/store/form.ts
@@ -1,8 +1,22 @@
-import type { StateCreator } from "zustand";
-
 import type { OperationSlice } from "../feature/operation/slice";
+
+import type { StateCreator } from "zustand";
 import type { ValidationPhase } from "../lib/get-validation-error-message";
-import type { ValidationSlice } from "./validation";
+
+export type ValidationSlice = {
+  validationPhase: ValidationPhase;
+  setValidationPhase: (validationPhase: ValidationPhase) => void;
+};
+
+export const createValidationSlice: StateCreator<
+  ValidationSlice,
+  [],
+  [],
+  ValidationSlice
+> = (set) => ({
+  validationPhase: "onChange",
+  setValidationPhase: (validationPhase) => set({ validationPhase }),
+});
 
 export type CommonFormSlice<T> = ValidationSlice &
   OperationSlice & {
