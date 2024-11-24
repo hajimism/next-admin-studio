d8c3e684fea290ae3e4cedd5693d0560236bae88
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-25 07:02:14 +0900

    feat: add administrator avatar component and integrate into header

--- src/app/(general)/_layout/header.tsx
+++ src/app/(general)/_layout/header.tsx
@@ -2,10 +2,13 @@ import { Flex, Text } from "@mantine/core";
 
 import { ENV_CLIENT } from "@/env/client";
 
+import { AdministratorAvaterContainer } from "@/model/administrator/components/avater/container";
+
 export const Header = () => {
   return (
     <Flex justify="space-between" align="center" py="xs" px="md">
-      <Text>{ENV_CLIENT.NEXT_PUBLIC_APP_TITLE}</Text>
+      <Text fw="bold">{ENV_CLIENT.NEXT_PUBLIC_APP_TITLE}</Text>
+      <AdministratorAvaterContainer />
     </Flex>
   );
 };
