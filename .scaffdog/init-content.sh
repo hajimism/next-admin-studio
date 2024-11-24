33f17405f3f6054a88909d61c7369219e0d12db4
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 09:36:44 +0900

    feat: enhance scaffdog init script and add new npm scripts for model generation

--- .scaffdog/init-content.sh
+++ .scaffdog/init-content.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "ようこそ Next Admin Studio へ"
+model_name="$1"   # モデル名 例: message
+path="$2"         # パス 例: messages
+content_name="$3" # コンテンツ名 例: メッセージ
+
+echo "generatemodel: $model_name $path $content_name"
