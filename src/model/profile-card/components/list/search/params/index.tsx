3cc1c17d03ac86878d230232bc2d7573e87324ae
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 14:12:37 +0900

    refactor: remove unused createSearchParamsCache import for improved clarity

--- src/model/profile-card/components/list/search/params/index.tsx
+++ src/model/profile-card/components/list/search/params/index.tsx
@@ -1,18 +1,9 @@
-import {
-  createSearchParamsCache,
-  type inferParserType,
-  parseAsInteger,
-  parseAsString,
-} from "nuqs/server";
+import { type inferParserType, parseAsInteger, parseAsString } from "nuqs";
 
 export const profileCardSearchParams = {
   q: parseAsString.withDefault(""),
   ln: parseAsInteger.withDefault(0),
 };
-
-export const profileCardSearchParamsCache = createSearchParamsCache(
-  profileCardSearchParams,
-);
 
 export type ProfileCardSearchParams = inferParserType<
   typeof profileCardSearchParams
