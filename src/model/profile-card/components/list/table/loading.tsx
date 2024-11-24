f66b1ff665cb1ee9430359c3b2c83cecd86c9591
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 11:45:49 +0900

    feat: add generateSequentialArray utility and refactor loading components to use it

--- src/model/profile-card/components/list/table/loading.tsx
+++ src/model/profile-card/components/list/table/loading.tsx
@@ -4,30 +4,28 @@ import { Skeleton, Table } from "@mantine/core";
 
 import { DEFAULT_PAGE_SIZE } from "@/model/common/components/pagination/const";
 
+import { generateSequentialArray } from "@/common/lib/array";
+
 import { ProfileCardTableHead } from "./head";
 
-export const ProfileCardTableListLoading = () => {
-  const ITEMS = Array.from({ length: DEFAULT_PAGE_SIZE }, (_, index) => index);
+export const ProfileCardTableListLoading = () => (
+  <Table>
+    <ProfileCardTableHead />
 
-  return (
-    <Table>
-      <ProfileCardTableHead />
-
-      <Table.Tbody>
-        {ITEMS.map((v) => (
-          <Table.Tr key={v}>
-            <Table.Td py="sm" px="lg" className="text-center text-gray-6">
-              <Skeleton height={15} width={12} radius="md" />
-            </Table.Td>
-            <Table.Td py="sm">
-              <Skeleton height={15} width={400} radius="md" />
-            </Table.Td>
-            <Table.Td py="sm">
-              <Skeleton height={15} width={112} radius="md" />
-            </Table.Td>
-          </Table.Tr>
-        ))}
-      </Table.Tbody>
-    </Table>
-  );
-};
+    <Table.Tbody>
+      {generateSequentialArray(DEFAULT_PAGE_SIZE).map((v) => (
+        <Table.Tr key={v}>
+          <Table.Td py="sm" px="lg" className="text-center text-gray-6">
+            <Skeleton height={15} width={12} radius="md" />
+          </Table.Td>
+          <Table.Td py="sm">
+            <Skeleton height={15} width={400} radius="md" />
+          </Table.Td>
+          <Table.Td py="sm">
+            <Skeleton height={15} width={112} radius="md" />
+          </Table.Td>
+        </Table.Tr>
+      ))}
+    </Table.Tbody>
+  </Table>
+);
