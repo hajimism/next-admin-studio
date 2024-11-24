c8d768f2e174def65735dade1d5561711a6eb425
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-24 10:45:45 +0900

    refactor: rename provider to layout and simplify ProfileCardDetailPage component

--- src/app/(general)/profile-cards/[id]/layout.tsx
+++ src/app/(general)/profile-cards/[id]/layout.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import type { FC, ReactNode } from "react";
+import { type ReactNode, use } from "react";
 
 import { profileCardConverter } from "@/model/profile-card/components/form-with-preview/converter";
 import { useProfileCardServerState } from "@/model/profile-card/components/form-with-preview/server-state/hook";
 import { ProfileCardFormStoreProvider } from "@/model/profile-card/components/form-with-preview/store/provider";
 
-export const ProfileCardFormProvider: FC<{
-  id: string;
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
   children: ReactNode;
-}> = ({ id, children }) => {
+};
+
+export default function ProfileCardDetailLayout(props: Props) {
+  const { id } = use(props.params);
   const serverState = useProfileCardServerState(id);
   const formValue = profileCardConverter.toClient(serverState);
 
   return (
     <ProfileCardFormStoreProvider initialState={formValue}>
-      {children}
+      {props.children}
     </ProfileCardFormStoreProvider>
   );
-};
+}
--- src/app/(general)/profile-cards/[id]/page.tsx
+++ src/app/(general)/profile-cards/[id]/page.tsx
@@ -1,19 +1,5 @@
 import { ProfileCardFormWithPreview } from "@/model/profile-card/components/form-with-preview";
 
-import { ProfileCardFormProvider } from "./provider";
-
-type Props = {
-  params: Promise<{
-    id: string;
-  }>;
-};
-
-export default async function ProfileCardDetailPage(props: Props) {
-  const { id } = await props.params;
-
-  return (
-    <ProfileCardFormProvider id={id}>
-      <ProfileCardFormWithPreview />
-    </ProfileCardFormProvider>
-  );
+export default function ProfileCardDetailPage() {
+  return <ProfileCardFormWithPreview />;
 }
