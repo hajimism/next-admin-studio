841d7cdddd9f2111a9d5032fcc47a34cee1b3fe3
hajimism <h_nakagawa@yumemi.co.jp>
2024-11-25 11:43:10 +0900

    refactor: add helper to extract model name from path in scaffdog config

--- .scaffdog/config.js
+++ .scaffdog/config.js
@@ -1,3 +1,12 @@
 export default {
-  files: ['*'],
+  files: ["*"],
+  helpers: [
+    {
+      // model名をパスから抜き出す
+      extractModel: (_context, value) => {
+        const match = value.match(/model\/([^\/]+)\//);
+        return match ? match[1] : "";
+      },
+    },
+  ],
 };
